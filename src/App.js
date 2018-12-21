c1617975d22fe6eccefb5c564e62c6cea5db8e2d
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-21 14:02:19 +0100

    creating toggle component with useState hook

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import React, { useState } from 'react';
 const App = () => {
 
   const [count, setCountState] = useState(0);
+  const [isOn, setIsOn] = useState(false);
 
   const incrementCount = () => {
     setCountState(prevCount => prevCount + 1)
     // prevCount does the same than prevState in Class components
   }
 
+  const toggleLight = () => {
+    setIsOn(prevIsOn => !prevIsOn);
+    // prevIsOn prevents bad behaviour with asynchronous timing
+  }
+
   return (
-    <button onClick={incrementCount}>
-      {count === 0 ? 'Click me, please' : `I was clicked ${count} times`}
-    </button>
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}>
+        {count === 0 ? 'Click me, please' : `I was clicked ${count} times`}
+      </button>
+      <h2>Toggle light</h2>
+      <img
+        src={isOn ? 'https://icon.now.sh/highlight/fd0' : 'https://icon.now.sh/highlight/aaa'}
+        alt="Flashlight"
+        style={{ height: '100px', width: '100px' }} onClick={toggleLight} />
+    </>
   )
 }
 
