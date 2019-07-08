cf4b93825d9a519248efb8bf58f30b86cb4a0597
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-08 23:51:59 +0200

    useEfects hooks with side effects resolved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
 
   const [count, setCountState] = useState(0);
   const [isOn, setIsOn] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null })
+
+  useEffect(() => {
+    // this function is executed after every render
+    // could be equivalent to componentDidMount() + componentDidUpdate()
+    document.title = `You have clicked ${count} times`
+    //* this is the right place to register an Event Listener
+    //* The issue is that we don't want to register this event everytime the
+    //* component is updated (we have to avoid side effects)
+    window.addEventListener('mousemove', mouseMoveHandler);
+
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveHandler);
+    }
+    //* On this way we avoid an infinite loop
+  }, [count]);
 
   const incrementCount = () => {
     setCountState(prevCount => prevCount + 1)
@@ -13,6 +29,13 @@ const App = () => {
   const toggleLight = () => {
     setIsOn(prevIsOn => !prevIsOn);
     // prevIsOn prevents bad behaviour with asynchronous timing
+  }
+
+  const mouseMoveHandler = event => {
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY
+    })
   }
 
   return (
@@ -26,6 +49,9 @@ const App = () => {
         src={isOn ? 'https://icon.now.sh/highlight/fd0' : 'https://icon.now.sh/highlight/aaa'}
         alt="Flashlight"
         style={{ height: '100px', width: '100px' }} onClick={toggleLight} />
+      <h2>Mouse Position</h2>
+      <p>X position: {mousePosition.x}</p>
+      <p>Y position: {mousePosition.y}</p>
     </>
   )
 }
